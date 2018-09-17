857a1a16296341883321d4b32ebb4a1d3711e19d
Erick Muñoz <lilrick1014@gmail.com>
2018-09-16 21:04:52 -0500

    codigo redux

--- src/index.js
+++ src/index.js
@@ -2,13 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
 import './index.css';
+import thunk from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import {Provider} from 'react-redux';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './rootReducer';
+
+const store = createStore (
+    rootReducer, 
+    composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(    
 <BrowserRouter>
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 </BrowserRouter>
 , document.getElementById('root'));
 registerServiceWorker();
--- src/vistas/Login/index.js
+++ src/vistas/Login/index.js
@@ -1,13 +1,10 @@
 import React from "react";
 import FormLog from "../form/login";
-
-
-
+import Proptypes from 'prop-types';
+import {connect} from 'react-redux';
+import {login} from "../../actions/auth";
 class Login extends React.Component {
-    submit = data =>{
-        console.log("submit");
-        console.log(data);
-    }
+    submit = data => this.props.login(data).then(() => this.props.history.push("/"));
     render(){
         return(
         <div>
@@ -17,5 +14,11 @@ class Login extends React.Component {
         );
     }
 }
+Login.propTypes = {
+    history: Proptypes.shape({
+        push: Proptypes.func.isRequired
+    }).isRequired,
+    login:Proptypes.func.isRequired
+};
 
-export default Login;
+export default connect(null,{login})(Login);
